4bc4ad6907643717e4bce4431c626e289839e9f0
adyasa974 <adbarik60@gmail.com>
2026-09-08 15:03:32 +0530

    pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blog from "./Pages/Blog/Blog";
+import BlogDetails from "./Pages/BlogDetails/BlogDetails";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Faq from "./Pages/Faq/Faq";
 
 const App = () => {
   return (
@@ -7,9 +11,10 @@ const App = () => {
     
 
       <Routes>
-       
-
-
+       <Route path="/blog" element={<Blog/>}/>
+<Route path="/blogdetails" element={<BlogDetails/>}/>
+<Route path="/contactus" element={<ContactUs/>}/>
+<Route path="/faq" element={<Faq/>}/>
       </Routes>
     </BrowserRouter>
   );
